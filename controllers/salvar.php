6a91be344ab33e0c6364304f0b5ae04a8b48b428
Max Hertel <maxherteljr@gmail.com>
2025-12-11 14:50:37 -0300

    atulizar

--- controllers/salvar.php
+++ controllers/salvar.php
@@ -3,6 +3,7 @@
     var_dump($_POST);
 
     //include da classe
-    //criar objeto com o daddos do post
+    //criar objeto com o dados do post
+    //dentro da classe criar um metodo novo chamado salvar que salva no arquivo
     // criar um medoto para salva no CSV    
 ?>
