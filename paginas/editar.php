14f57d05099855a080f7a25c0ac87593fbb79c06
Max Hertel <maxherteljr@gmail.com>
2025-12-12 16:18:11 -0300

    parte de edicao criada

--- paginas/editar.php
+++ paginas/editar.php
@@ -1,16 +1,44 @@
 <?php
-    include '../includes/header.php'
-    //logica para salvar pessoas usando a classe
+include '../includes/header.php';
+$pessoaID = $_GET['pessoaid'];
+
+// define que o caminho e igual a banco de dados
+$caminho = '../banco/pessoas.csv';
+//pessoas igual a dados cadastrados
+$pessoa = [];
+//verifica se existe o arquivo
+if (file_exists($caminho)) {
+    //abre o arquvo
+    $arquivo = fopen($caminho, 'r');
+    //organiza o arquivo
+    $cabecalho = fgetcsv($arquivo, 0, ';');
+    //percorre a linha
+    while (($linha = fgetcsv($arquivo, 0, ';')) !== false) {
+        if($linha[0] == $pessoaID) {
+        $pessoa = $linha;
+        }
+    }
+    //fecha o arquivo
+    fclose($arquivo);
+}
+
+var_dump($pessoa);
 ?>
-  <div class="container mt-5">
-            <h2 class="mb-4 text-center">Editar</h2>
+<div class="container mt-5">
+    <h2 class="mb-4 text-center">Editar</h2>
 
-            <div class="row g-4">
-                
-               
+    <div class="row g-4">
+<form class="row g-4 p-4 bg-white shadow rounded" method="POST" action="/controllers/atualizar.php">
 
-            </div>
+  <div class="col-md-6">
+            <label for="nomeCompleto" class="form-label">Nome Completo</label>
+            <input type="text" class="form-control" id="nomeCompleto" name="nomeCompleto" value="<?=$pessoa[1]?>" required>
         </div>
+
+</form>
+
+    </div>
+</div>
 <?php
-    include '../includes/footer.php'
-?>
+include '../includes/footer.php'
+    ?>
--- paginas/listar.php
+++ paginas/listar.php
@@ -1,16 +1,71 @@
-<?php
-    include '../includes/header.php'
-    //logica para salvar pessoas usando a classe
-?>
-  <div class="container mt-5">
-            <h2 class="mb-4 text-center">Listar</h2>
+<?php include '../includes/header.php'; ?>
 
-            <div class="row g-4">
-                
-               
+<h2>Lista de Pessoas</h2>
 
-            </div>
-        </div>
+
 <?php
-    include '../includes/footer.php'
+// define que o caminho e igual a banco de dados
+$caminho = '../banco/pessoas.csv';
+//pessoas igual a dados cadastrados
+$pessoas = [];
+//verifica se existe o arquivo
+if (file_exists($caminho)) {
+    //abre o arquvo
+    $arquivo = fopen($caminho, 'r');
+    //organiza o arquivo
+    $cabecalho = fgetcsv($arquivo, 0, ';');
+    //percorre a linha
+    while (($linha = fgetcsv($arquivo, 0, ';')) !== false) {
+        $pessoas[] = $linha;
+    }
+    //fecha o arquivo
+    fclose($arquivo);
+}
+
 ?>
+
+
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">nome</th>
+            <th scope="col">data nascimento</th>
+            <th scope="col">genero</th>
+            <th scope="col">estadoCivil</th>
+            <th scope="col">cpf</th>
+            <th scope="col">rg</th>
+            <th scope="col">email</th>
+            <th scope="col">telefone</th>
+            <th scope="col">celular</th>
+            <th scope="col">altura</th>
+            <th scope="col">peso</th>
+            <th scope="col">rua</th>
+            <th scope="col">numero rua</th>
+            <th scope="col">bairro</th>
+            <th scope="col">cidade</th>
+            <th scope="col">estado</th>
+            <th scope="col">cep</th>
+            <th scope="col">D</th>
+            <th scope="col">Date cadastro</th>
+            <th scope="col">Date atualização</th>
+            <th scope="col">Ação</th>
+        </tr>
+
+    <tbody>
+        <?php
+        //compara pessoas com linha
+        foreach ($pessoas as $linha): ?>
+            <tr>
+                <?php //compara pessoa com valor
+                    foreach ($linha as $valor): ?>
+                    <td><?=//prenche com o valor
+                                $valor ?></td>
+                <?php endforeach; ?>
+                <td><a href="/paginas/editar.php?pessoaid=<?php echo ($linha[0]) ?>" class="btn btn-primary">Editar</a></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<?php include '../includes/footer.php'; ?>
