8b8ed0869c1c7104dba5c395978bab74cfde3117
Max Hertel <maxherteljr@gmail.com>
2026-01-07 14:24:50 -0300

    criado o metodo salvar com o banco de dados1

--- controllers/atualizar.php
+++ controllers/atualizar.php
@@ -13,18 +13,9 @@ $idade = $hoje->diff($dataNascimento)->y;
         $_POST['nomeCompleto'],     // 1
         $_POST['id'],               // 2
         $_POST['dataNascimento'],   // 3
-        $_POST['genero'],           // 4
-        $_POST['estadoCivil'],      // 5
-        $_POST['cpf'],              // 6
-        $_POST['rg'],               // 7
-        $_POST['email'],            // 8
-        $_POST['telefone'],         // 9
-        $_POST['celular'],          // 10
-        $_POST['altura'],           // 11
-        $_POST['peso'],             // 12
-        $_POST['endereco']          // 13
+        $_POST['genero']          // 4
     );
-    $pessoa->atualizarcsv($_POST['id']);
+    $pessoa->atualizar($_POST['id']);
 
 
 header("Location: /paginas/cadastrar.php");
